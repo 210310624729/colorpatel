702141d60bee583a4bfe88acfd5725bcb84d115c
Ashim Banerjee <125278221+210310624729@users.noreply.github.com>
2023-10-18 20:19:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,79 +12,83 @@ function buttonAnimation(color)
 const colorButtons = document.querySelectorAll('.item');
 
 colorButtons.forEach(button => {
-  button.addEventListener("click", function () {
-    const colorId = this.id;
-    let colorCode;
+    button.addEventListener('click', function() {
+        const colorId = this.id;
+        let colorCode;
+        
+        switch(colorId) {
+            case 'violet':
+                colorCode = 'violet';
+                break;
+            case 'blue':
+                colorCode = 'blue';
+                break;
+            case 'green':
+                colorCode = 'green';
+                break;
+            case 'yellow':
+                colorCode = 'yellow';
+                break;
+            case 'orange':
+                colorCode = 'orange';
+                break;
+            case 'red':
+                colorCode = 'red';
+                break;
+            case 'black':
+                colorCode = 'black';
+                break;
+            case 'grey':
+                colorCode = 'grey';
+                break;
+            case 'brown':
+                colorCode = 'brown';
+                break;
+            default:
+                colorCode = 'white'; // default color if none matched
+        }
 
-    switch (colorId) {
-      case "violet":
-        colorCode = "violet";
-        break;
-      case "blue":
-        colorCode = "blue";
-        break;
-      case "green":
-        colorCode = "green";
-        break;
-      case "yellow":
-        colorCode = "yellow";
-        break;
-      case "orange":
-        colorCode = "orange";
-        break;
-      case "red":
-        colorCode = "red";
-        break;
-      case "black":
-        colorCode = "black";
-        break;
-      case "grey":
-        colorCode = "grey";
-        break;
-      case "brown":
-        colorCode = "brown";
-        break;
-      default:
-        colorCode = "white"; // default color if none matched
-    }
+        changeColor(colorCode);
+        buttonAnimation(colorCode);
 
-    changeColor(colorCode);
-    buttonAnimation(colorCode);
-  });
-  // Define a variable to store the selected color
-  let selectedColor = localStorage.getItem("selectedColor") || "white";
 
-  function changeColor(color) {
-    document.body.style.backgroundColor = color;
-    selectedColor = color; // Update the variable
-    localStorage.setItem("selectedColor", color);
-  }
-
-  function buttonAnimation(color) {
-    var activeButton = document.querySelector("#" + color);
-    activeButton.classList.add("pressed");
-    setTimeout(function () {
-      activeButton.classList.remove("pressed");
-    }, 100);
-  }
-
-  const colorButtons = document.querySelectorAll(".item");
-
-  colorButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      const colorId = this.id;
-      let colorCode = colorId;
-
-      changeColor(colorCode);
-      buttonAnimation(colorCode);
     });
-  });
+    let selectedColors = JSON.parse(localStorage.getItem("selectedColors")) || [
+      "white",
+    ];
 
-  document.addEventListener("DOMContentLoaded", function () {
-    if (selectedColor) {
-      changeColor(selectedColor);
+    function changeColor(color) {
+      document.body.style.backgroundColor = color;
+      selectedColors.push(color);
+      localStorage.setItem("selectedColors", JSON.stringify(selectedColors));
     }
-  });
+
+    function buttonAnimation(color) {
+      var activeButton = document.querySelector("#" + color);
+      activeButton.classList.add("pressed");
+      setTimeout(function () {
+        activeButton.classList.remove("pressed");
+      }, 100);
+    }
+
+    const colorButtons = document.querySelectorAll(".item");
+
+    colorButtons.forEach((button) => {
+      button.addEventListener("click", function () {
+        const colorId = this.id;
+        let colorCode = colorId;
+
+        changeColor(colorCode);
+        buttonAnimation(colorCode);
+      });
+    });
+
+    document.addEventListener("DOMContentLoaded", function () {
+      if (selectedColors.length > 0) {
+        changeColor(selectedColors[selectedColors.length - 1]);
+      }
+    });
+
 });
 
 
